2fb7c71fca0d916cc4dc852ac98e324f39478122
theguy000 <istiakm30@gmail.com>
2026-04-08 22:07:42 +0600

    feat(mpv): expose mpv availability check via IPC for settings UI

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,9 +1,9 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 import Updater from "../core/Updater";
 import DiscordPresence from "../core/DiscordPresence";
 import { discordTracker } from "./ui/discordTracker";
 import EmbeddedSubtitles from "../utils/EmbeddedSubtitles";
-import { STORAGE_KEYS } from "../constants";
+import { STORAGE_KEYS, IPC_CHANNELS } from "../constants";
 
 // plugin API bridges
 import { alertAPI } from './api/alert';
@@ -26,6 +26,7 @@ export const stremioEnhancedAPI = {
     ...applyThemeAPI,
     ...gpuRendererAPI,
     ...externalPlayerAPI,
+    mpvIsAvailable: () => ipcRenderer.invoke(IPC_CHANNELS.MPV_IS_AVAILABLE),
 };
 
 contextBridge.exposeInMainWorld('StremioEnhancedAPI', stremioEnhancedAPI);
